4c0b001ce962d7c5deffe221c7c81d94442c9fe5
Terence Copestake <terence.copestake@gmail.com>
2021-09-11 11:15:49 +0100

    Tidied some things up; added tco_patch_function demo

--- src/tailcall.c
+++ src/tailcall.c
@@ -3,7 +3,6 @@
 #include <stddef.h>
 #include "php.h"
 #include "zend_extensions.h"
-#include "zend_ast.h"
 
 /* Handle platform-specific hax */
 
@@ -21,77 +20,66 @@ static void (*zend_ast_process_copy)(zend_ast*);
 
 FILE *dbg;
 
-/* Custom AST handler */
+/*
+ * ...
+ *
+ */
+void tco_patch_function(zend_ast *ast)
+{
+    zend_ast_decl *declaration = (zend_ast_decl *) ast;
+
+    fprintf(dbg, "tco_patch_function: ");
+    fwrite(ZSTR_VAL(declaration->name), ZSTR_LEN(declaration->name), 1, dbg);
+    fprintf(dbg, "\n");
+    fflush(dbg);
+}
+
+/*
+ * Custom AST handler
+ *
+ * The general idea here is:
+ *
+ * 1. Recursively explore all child nodes.
+ * 2. Once child nodes have been explored...
+ * 3. Check whether current node is a function/method declaration.
+ * 4. If so, run it through tco_patch_function()
+ *
+ */
 void tco_walk_ast(zend_ast *ast)
 {
-    fflush(dbg);
-    fputs("\n tco_walk_ast: \n", dbg);
-    fprintf(dbg, "(node: %d)\n", ast->kind);
-    fflush(dbg);
+    // Part 1: Explore child nodes.
+
+    uint32_t assumed_children = 0;
+    zend_ast **child_nodes;
+
+    // Get children/counts for either lists or types w/ 1+ children.
+
+    if (zend_ast_is_list(ast)) {
+        zend_ast_list *list = zend_ast_get_list(ast);
+
+        assumed_children = list->children;
+        child_nodes = list->child;
+    } else if (ast->kind >= (1 << ZEND_AST_NUM_CHILDREN_SHIFT)) {
+        assumed_children = zend_ast_get_num_children(ast);
+        child_nodes = ast->child;
+    }
+
+    // Walk through any and all child nodes.
+
+    if (assumed_children) {
+        for (uint32_t i = 0; i < assumed_children; i++) {
+            if (child_nodes[i]) {
+                tco_walk_ast(child_nodes[i]);
+            }
+        }
+    }
+
+    // Part 2: Extra magic for function/method declarations.
 
     switch (ast->kind) {
-        // case ZEND_AST_CALL:
-
-
-        /*case ZEND_AST_STMT_LIST:
-            fprintf(dbg, "(ZEND_AST_STMT_LIST)\n");
-            fflush(dbg);
-
-            zend_ast_list *list = zend_ast_get_list(ast);
-
-            for (int i = 0; i < list->children; i++) {
-                tco_walk_ast(list->child[i]);
-            }
-
-            break;*/
-
+        case ZEND_AST_METHOD:
         case ZEND_AST_FUNC_DECL:
-            // (It seems there's no inline function for casting to zend_ast_decl?)
-
-            zend_ast_decl *function = (zend_ast_decl *) ast;
-
-            fprintf(dbg, "(ZEND_AST_FUNC_DECL)\n");
-            fwrite(ZSTR_VAL(function->name), ZSTR_LEN(function->name), 1, dbg);
-            fflush(dbg);
-
-            break;
-
-        default:
-            // Generic handler for other types.
-
-            uint32_t assumed_children = 0;
-            zend_ast **child_nodes;
-
-            fprintf(dbg, "(generic)\n");
-            fflush(dbg);
-
-            if (zend_ast_is_list(ast)) {
-                fprintf(dbg, "(is a list)\n");
-                fflush(dbg);
-
-        		zend_ast_list *list = zend_ast_get_list(ast);
-
-                assumed_children = list->children;
-                child_nodes = list->child;
-            } else if (ast->kind >= (1 << ZEND_AST_NUM_CHILDREN_SHIFT)) {
-                fprintf(dbg, "(has children)\n");
-                fflush(dbg);
-
-                assumed_children = zend_ast_get_num_children(ast);
-                child_nodes = ast->child;
-            }
-
-            // Walk through any and all child nodes.
-
-            if (assumed_children) {
-                fprintf(dbg, "(child count: %d)\n", assumed_children);
-
-                for (uint32_t i = 0; i < assumed_children; i++) {
-                    if (child_nodes[i]) {
-                        tco_walk_ast(child_nodes[i]);
-                    }
-                }
-            }
+            tco_patch_function(ast);
     }
 }
 
