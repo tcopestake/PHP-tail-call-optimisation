33e2f2a090b38a290b89086e0aa5eb521fd3b352
Terence Copestake <terence.copestake@gmail.com>
2021-09-12 14:56:39 +0100

    Can identify classes now, etc.

--- src/tailcall.c
+++ src/tailcall.c
@@ -17,17 +17,24 @@
 
 /* Some variables/types/etc. */
 
-typedef struct _tco_decl_returns_link {
+typedef struct _tco_callable_returns_link {
 	zend_ast **siblings_and_self;
 	uint32_t self_index;
     zend_ast *self_node;
-    struct _tco_decl_returns_link *previous;
-} tco_decl_returns_link;
+    struct _tco_callable_returns_link *previous;
+} tco_callable_returns_link;
 
-typedef struct _tco_decl_context {
-	zend_ast_decl *declaration;
-    tco_decl_returns_link *returns_tail;
-} tco_decl_context;
+typedef struct _tco_callable_context {
+	zend_ast_decl *callable_decl;
+    tco_callable_returns_link *returns_tail;
+} tco_callable_context;
+
+typedef struct _tco_context {
+	zend_ast_decl *class_decl;
+	tco_callable_context *callable_context;
+} tco_context;
+
+tco_context tco_global_context;
 
 static void (*zend_ast_process_copy)(zend_ast*);
 
@@ -45,64 +52,64 @@ void tco_cleanup()
 /*
  * Allocate memory for a new declaration context, etc.
  */
-tco_decl_context *tco_create_decl_context(zend_ast_decl *declaration)
+tco_callable_context *tco_create_callable_context(zend_ast_decl *callable_decl)
 {
-    tco_decl_context *decl_context = malloc(sizeof(tco_decl_context));
+    tco_callable_context *callable_context = malloc(sizeof(tco_callable_context));
 
-    decl_context->declaration = declaration;
-    decl_context->returns_tail = NULL;
+    callable_context->callable_decl = callable_decl;
+    callable_context->returns_tail = NULL;
 
-    return decl_context;
+    return callable_context;
 }
 
 /*
  * Allocate memory for a new node in the returns list.
  */
-tco_decl_returns_link *tco_alloc_decl_returns_link()
+tco_callable_returns_link *tco_alloc_callable_returns_link()
 {
-    return malloc(sizeof(tco_decl_returns_link));
+    return malloc(sizeof(tco_callable_returns_link));
 }
 
 /*
  * Frees all memory allocated for (and associated with) a given context.
  * (This includes the return nodes btw.)
  */
-void tco_free_decl_context(tco_decl_context *decl_context)
+void tco_free_callable_context(tco_callable_context *callable_context)
 {
     // Free memory allocated for returns.
 
-    tco_decl_returns_link *current_return = decl_context->returns_tail;
-    tco_decl_returns_link *tmp;
+    tco_callable_returns_link *current_return = callable_context->returns_tail;
+    tco_callable_returns_link *tmp;
 
     while (current_return) {
         fprintf(dbg, "(freeing current_return)\n");
         fflush(dbg);
 
-        // Save the pointer before we free the memory it's stored in.
+        // Save the previous pointer before we free the memory it's stored in.
 
         tmp = current_return->previous;
 
         free(current_return);
 
-        // Update pointer to next (technically previous) return node.
+        // Update pointer to previous (technically next) return node.
 
         current_return = tmp;
     }
 
     // Now free the context itself.
 
-    free(decl_context);
+    free(callable_context);
 }
 
 /*
  * Adds a new return node/link in the given context.
  */
-void tco_decl_context_add_return(
-    tco_decl_context *decl_context,
+void tco_callable_context_add_return(
+    tco_callable_context *callable_context,
     zend_ast **siblings_and_self,
     uint32_t self_index
 ) {
-    tco_decl_returns_link *new_returns_link = tco_alloc_decl_returns_link();
+    tco_callable_returns_link *new_returns_link = tco_alloc_callable_returns_link();
 
     // Set return link node values.
 
@@ -112,17 +119,30 @@ void tco_decl_context_add_return(
 
     // Point new node to the current tail & update tail pointer.
 
-    new_returns_link->previous = decl_context->returns_tail;
+    new_returns_link->previous = callable_context->returns_tail;
 
-    decl_context->returns_tail = new_returns_link;
+    callable_context->returns_tail = new_returns_link;
 }
 
 /*
  * ...
  */
-void tco_patch_declaration(tco_decl_context *decl_context)
+void tco_patch()
 {
-    zval declaration_name_zval;
+    zval callable_name_zval;
+
+    zend_ast_decl *class_decl = tco_global_context.class_decl;
+    tco_callable_context *callable_context = tco_global_context.callable_context;
+
+    if (class_decl) {
+        fprintf(dbg, "Class: ");
+        fwrite(ZSTR_VAL(class_decl->name), ZSTR_LEN(class_decl->name), 1, dbg);
+        fprintf(dbg, "\n");
+        fflush(dbg);
+    } else {
+        fprintf(dbg, "(No class context)\n");
+        fflush(dbg);
+    }
 
     // ...
 
@@ -137,24 +157,19 @@ void tco_patch_declaration(tco_decl_context *decl_context)
 
     zend_ast *echo = zend_ast_create(ZEND_AST_ECHO, (zend_ast *)arg);
 
+    // ...
 
 
 
 
-
-
-
-
-
-    zend_ast_decl *declaration = decl_context->declaration;
 
     // We need the declaration name as a zval for the comparison functions (I think).
 
-    ZVAL_STR(&declaration_name_zval, declaration->name);
+    ZVAL_STR(&callable_name_zval, callable_context->callable_decl->name);
 
     // Explore returns (if applicable).
 
-    tco_decl_returns_link *current_return = decl_context->returns_tail;
+    tco_callable_returns_link *current_return = callable_context->returns_tail;
 
     for (; current_return; current_return = current_return->previous) {
         zend_ast *return_node = current_return->self_node;
@@ -167,12 +182,18 @@ void tco_patch_declaration(tco_decl_context *decl_context)
             case ZEND_AST_CALL:
                 if (
                     string_case_compare_function(
-                        &declaration_name_zval,
+                        &callable_name_zval,
                         &((zend_ast_zval *) return_value->child[0])->val
-                    ) == 0
+                    ) != 0
                 ) {
-                    // (Is recursive)
+                    // Not recursive; ignore it.
+
+                    continue;
                 }
+
+                // If we're here, this is a recursive function call.
+
+
 
                 break;
 
@@ -210,52 +231,80 @@ void tco_patch_declaration(tco_decl_context *decl_context)
 void tco_walk_ast(
     zend_ast *ast,
     zend_ast **siblings_and_self,
-    uint32_t self_index,
-    tco_decl_context *decl_context
+    uint32_t self_index
 ) {
-    // Part 1: Explore child nodes.
+    // This may get used if global contextual values are changed.
+    // (and subsequently need to be restored)
 
+    tco_context context_backup;
+    bool restore_class_decl = false;
+
+    // Part 1: Identify current node; set any values of interest;
+    // & configure values for exploring this node's children later.
+
+    zend_ast_decl *callable_decl = NULL;
     uint32_t assumed_children = 0;
     zend_ast **child_nodes;
-    zend_ast_decl *declaration = NULL;
-
-    // Get children/counts for declarations, lists and types w/ 1+ children.
 
     switch (ast->kind) {
+        case ZEND_AST_CLASS:
+            // We're about to switch to a new class declaration - so we'll back up the current declaration.
+
+            context_backup.class_decl = tco_global_context.class_decl;
+
+            restore_class_decl = true;
+
+            // Switch to new class declaration.
+
+            tco_global_context.class_decl = (zend_ast_decl *) ast;
+
+            // Configure child node values, etc.
+
+            assumed_children = 4;
+            child_nodes = tco_global_context.class_decl->child;
+
+            break;
+
         case ZEND_AST_RETURN:
             fprintf(dbg, "(ast->kind == ZEND_AST_RETURN)\n");
             fflush(dbg);
 
             // While we're here, we'll collect this return in the current context.
+            // We're only interested in saving return values within a declaration.
 
-            tco_decl_context_add_return(decl_context, siblings_and_self, self_index);
+            if (tco_global_context.callable_context) {
+                tco_callable_context_add_return(
+                    tco_global_context.callable_context,
+                    siblings_and_self,
+                    self_index
+                );
+            } else {
+                fprintf(dbg, "(Skipping rando return)\n");
+                fflush(dbg);
+            }
 
             // Set values for exploring child nodes, etc.
 
             assumed_children = 1;
             child_nodes = ast->child;
 
-            // ...
-
-            fprintf(dbg, "Return child kind: %d (vs %d)\n", ast->child[0]->kind, ZEND_AST_CALL);
-            fflush(dbg);
-
-            // ...
-
             break;
 
     	case ZEND_AST_FUNC_DECL:
     	case ZEND_AST_METHOD:
-            declaration = (zend_ast_decl *) ast;
+            // We're about to switch to a new callable context - so we'll back up the current context.
 
-            // Create a new declaration context for this... declaration.
+            context_backup.callable_context = tco_global_context.callable_context;
 
-            decl_context = tco_create_decl_context(declaration);
+            // Create a new callable context for this... callable.
+
+            callable_decl = (zend_ast_decl *) ast;
+
+            tco_global_context.callable_context = tco_create_callable_context(callable_decl);
 
             // Fall-through here is intentional.
 
     	case ZEND_AST_CLOSURE:
-    	case ZEND_AST_CLASS:
             assumed_children = 4; // (Should we be calculating these values from the node/constant?)
             child_nodes = ((zend_ast_decl *) ast)->child;
 
@@ -275,26 +324,36 @@ void tco_walk_ast(
             }
     }
 
-    // Walk through any and all child nodes.
+    // Part 2: Explore any and all child nodes.
 
     if (assumed_children) {
         for (uint32_t i = 0; i < assumed_children; i++) {
             if (child_nodes[i]) {
-                tco_walk_ast(child_nodes[i], child_nodes, i, decl_context);
+                tco_walk_ast(child_nodes[i], child_nodes, i);
             }
         }
     }
 
-    // Part 2: Extra magic for function/method declarations.
+    // Part 3: Extra magic if this current node is a function/method declaration.
 
-    if (declaration) {
+    if (callable_decl) {
         // Patch it.
 
-        tco_patch_declaration(decl_context);
+        tco_patch();
 
         // Free the current declaration context; we don't need it anymore.
 
-        tco_free_decl_context(decl_context);
+        tco_free_callable_context(tco_global_context.callable_context);
+
+        // Restore the original callable context.
+
+        tco_global_context.callable_context = context_backup.callable_context;
+    }
+
+    // If context was changed, restore it.
+
+    if (restore_class_decl) {
+        tco_global_context.class_decl = context_backup.class_decl;
     }
 }
 
@@ -305,9 +364,14 @@ void tco_ast_process(zend_ast *ast)
 {
     dbg = fopen("G:/dev/tailcall/astlog.txt", "w");
 
+    // Init the global context.
+
+    tco_global_context.class_decl = NULL;
+    tco_global_context.callable_context = NULL;
+
     // (Have a guess what this does.)
 
-    tco_walk_ast(ast, NULL, 0, NULL);
+    tco_walk_ast(ast, NULL, 0);
 
     // ...
 
